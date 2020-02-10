dfbdd0cc3965f4f1d82638ca6e7ebeeca2e7b0fe
Lucas Antunes <lucasdanielantunes@gmail.com>
2020-02-10 16:08:03 -0300

    projeto finalizado

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,13 @@ import App from './App'
 
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
+import './config/mq'
+
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
-
-//Tem
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikx1Y2FzIEQuIEFudHVuZXMiLCJlbWFpbCI6Imx1Y2FzZGFuaWVsYW50dW5lc0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTgxMTMzMjgwLCJleHAiOjE1ODEzOTI0ODB9.1vEraTFrTsm0Yamw50558iRah_3uUerMUHhQfw_Xq0s'
 
 new Vue({
   store,
